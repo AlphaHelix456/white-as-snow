08bd3a83e97bb603976d2b0c238eba08ab008d81
Kyle Wong <kylewonga@gmail.com>
2017-01-27 18:32:34 -0800

    Added Snow

--- White_As_Snow/Assets/Scripts/MoveLoop.cs
+++ White_As_Snow/Assets/Scripts/MoveLoop.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class MoveLoop : MonoBehaviour {
 
+    public GameObject treeTarget;
     public float speed;
+    public Camera cam;
+    float camHeight;
+    float camWidth;
     private Vector3 loopLocation;
     private float myWidth;
     private Vector3 moveVector;
+
 	// Use this for initialization
 	void Start () {
         moveVector = Vector3.left * speed;
+        camHeight = 2f * cam.orthographicSize;
+        camWidth = camHeight * cam.aspect;
+        print(camWidth);
         myWidth = (float)GetComponent<RectTransform>().rect.width;
-        loopLocation = new Vector3(Screen.width + myWidth/2, transform.position.y,0);
+        loopLocation = treeTarget.transform.position;
+        
     }
 	
 	// Update is called once per frame
 	void Update () {
         transform.position += moveVector * Time.deltaTime;
-		if(transform.position.x < 0 - myWidth/2)
+        if (cam.WorldToScreenPoint(transform.position).x < -myWidth/2)
         {
+            //transform.position = new Vector2(cam.ViewportToWorldPoint(new Vector3(1,0,0)).x+myWidth/2,transform.position.y);
             transform.position = loopLocation;
         }
 	}
