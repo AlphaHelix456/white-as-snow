d3ff7df493205a0c5b4e943c12b70770c7ade823
Kyle Wong <kylewonga@gmail.com>
2017-01-27 16:39:05 -0800

    Created Main Menu Prototype
    Also created a couple of folders for sprites, scripts, and scenes

---
+++ White_As_Snow/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class MainMenuController : MonoBehaviour {
+
+    // Use this for initialization
+    private int menuState;
+    private const int MAIN_MENU = 0;  //Use these constants to represent menustate more easily
+    private const int CREDITS = 1;
+    void Start () {
+        menuState = MAIN_MENU;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void startGame()
+    {
+        //Runs when start button is pressed; switches to game scene
+
+        //SceneManager.LoadScene(SCENE_NAME);  Uncomment once scene is created
+    }
+    public void quitGame()
+    {
+        //Quits the game when quit button is pressed
+        Application.Quit();
+    }
+    public void goToCredits()
+    {
+        //Activates when credits is pressed, moves to credit screen
+        menuState = CREDITS;
+    }
+    public void returnToMainMenu()
+    {
+        //Activates when the back button is pressed, returns to main menu screen
+        menuState = MAIN_MENU;
+    }
+}
---
+++ White_As_Snow/Assets/Scripts/MoveLoop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLoop : MonoBehaviour {
+
+    public float speed;
+    private Vector3 loopLocation;
+    private float myWidth;
+    private Vector3 moveVector;
+	// Use this for initialization
+	void Start () {
+        moveVector = Vector3.left * speed;
+        myWidth = (float)GetComponent<RectTransform>().rect.width;
+        loopLocation = new Vector3(Screen.width + myWidth/2, transform.position.y,0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position += moveVector * Time.deltaTime;
+		if(transform.position.x < 0 - myWidth/2)
+        {
+            transform.position = loopLocation;
+        }
+	}
+}
