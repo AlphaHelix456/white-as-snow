3156cb3b5b431b36cd921a937f4213b28618080a
tolkwes <tolkwes@gmail.com>
2017-02-02 20:29:55 -0800

    Overworld Testing

---
+++ White_As_Snow/Assets/Scripts/WolfMovement.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WolfMovement : MonoBehaviour {
+	void Start () {
+		
+	}
+	
+	void Update () {
+        if(Input.GetKeyDown(KeyCode.W)) {
+            
+        }
+	}
+}
