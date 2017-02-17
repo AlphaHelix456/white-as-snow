61d3504dcf84f3e12a145bb3d059ed86c758a0ea
Neil Shah <warriors18672@gmail.com>
2017-02-16 19:54:01 -0800

    Movement for the Wolf Pack
    Each wolf follows the player.

--- White_As_Snow/Assets/Scripts/Follow.cs
+++ White_As_Snow/Assets/Scripts/Follow.cs
@@ -2,15 +2,42 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Follow : MonoBehaviour {
+public class NewBehaviourScript : MonoBehaviour {
 
-    private readonly float MAX_DISTANCE = 5f;
-	
+	private readonly float START_D = 7.1f;
+	private readonly float MIN_D = 5.5f;
+	private GameObject player;
+	private bool moving;
+	private float angle;
+
 	void Start () {
-		
+		player = GameObject.Find("wolf");
+		moving = false;
 	}
 	
+	// Update is called once per frame
 	void FixedUpdate () {
 		
 	}
+	private bool toMove()
+	{
+		if ((this.transform.position - player.transform.position).magnitude > START_D) 
+		{
+			return true;
+		} 
+		else return false;
+
+	}
+
+	private float getDist()
+	{
+		return((this.transform.position - player.transform.position).magnitude;
+
+	}
+	
+			private float.getAngle() //Returns the angle measured counterclockwise from the x-axis
+			{
+				return Mathf.Acos(Mathf.Abs(this.transform.position.x - player.transform.position.x)/getDist())*180/Mathf.PI;
+
+			}
 }
