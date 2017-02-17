49599bd6b887050f074a651183da3355fc1ad27f
InvalidCS <cjdipalma@yahoo.com>
2017-02-16 19:58:56 -0800

    Added stuff to follow code

--- White_As_Snow/Assets/Scripts/Follow.cs
+++ White_As_Snow/Assets/Scripts/Follow.cs
@@ -2,15 +2,47 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Follow : MonoBehaviour {
+public class Follow : MonoBehaviour
+{
 
-    private readonly float MAX_DISTANCE = 5f;
-	
-	void Start () {
-		
-	}
-	
-	void FixedUpdate () {
-		
-	}
+    private readonly float START_D = 7.1f;
+    private readonly float MIN_D = 5.5f;
+    private GameObject player;
+    private bool moving;
+    private float angle;
+
+    void Start()
+    {
+        player = GameObject.Find("wolf");
+        moving = false;
+    }
+
+    void FixedUpdate()
+    {
+        if (!moving && toMove())
+        {
+            angle = getAngle();
+        }
+    }
+
+    private bool toMove()
+    {
+        if (getDist() > START_D)
+        {
+            return true;
+        }
+        else return false;
+    }
+
+    private float getDist()
+    {
+        return (this.transform.position - player.transform.position).magnitude;
+    }
+
+    private float getAngle() //Returns the angle measured counterclockwise from the x-axis.
+    {
+        return Mathf.Acos(Mathf.Abs(this.transform.position.x - player.transform.position.x) / getDist()) * 180 / Mathf.PI;
+    }
 }
+
+   
