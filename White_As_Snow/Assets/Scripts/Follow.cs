7b2aa3b435da4703ea75116ce7d6f791779a6449
InvalidCS <cjdipalma@yahoo.com>
2017-02-18 01:53:26 -0800

    Revert "Follow code changes"
    This reverts commit 945e88acf2be051648598ec0f9438ba552638dd1.

--- White_As_Snow/Assets/Scripts/Follow.cs
+++ White_As_Snow/Assets/Scripts/Follow.cs
@@ -5,24 +5,19 @@ using UnityEngine;
 public class Follow : MonoBehaviour
 {
 
-    private readonly float START_D = 6.5f;
-    private readonly float MIN_D = 5f;
-    private readonly float FOLLOW_D = 4.2f;
+    private readonly float START_D = 7.1f;
+    private readonly float MIN_D = 5.5f;
     private readonly float SPEED = 4.5f;
     private GameObject player;
     private GameObject otherFollower;
     private Rigidbody2D rb;
-    public bool moving, needsToMove;
+    private bool moving;
     private float angle;
-    private bool[][] dirs = new bool[2][];
 
     void Start()
     {
-        for (int i = 0; i < 2; i++)
-            dirs[i] = new bool[4];
         player = GameObject.Find("wolf");
         moving = false;
-        needsToMove = false;
         rb = GetComponent<Rigidbody2D>();
         if (gameObject.name == "follower1")
         {
@@ -43,36 +38,31 @@ public class Follow : MonoBehaviour
 
         if (moving)
         {
-            if (getDist(player) <= MIN_D)
+            angle = getAngle();
+            if (angle < 67.5f)
             {
-                moving = false;
-                rb.velocity = new Vector2(0, 0);
-            }
-            else
-            {
-                dirs[0] = walkDir(player);
-                dirs[1] = walkDir(otherFollower);
-
-                //Left=0, right=1, up=2, down=3
-                //Movement in direction of player unless there is another wolf in the way
-                if(getDist(otherFollower) > FOLLOW_D || )
+                if (getXDiff() > 0) //If the follower is to the right of the player
                 {
-                    if (dirs[0][0])
-                        rb.velocity = new Vector2(-SPEED, rb.velocity.y);
-                    else if (dirs[0][1])
-                        rb.velocity = new Vector2(SPEED, rb.velocity.y);
-                    else
-                        rb.velocity = new Vector2(0, rb.velocity.y);
-                    if (dirs[0][2])
-                        rb.velocity = new Vector2(rb.velocity.x, SPEED);
-                    else if (dirs[0][3])
-                        rb.velocity = new Vector2(rb.velocity.x, -SPEED);
-                    else
-                        rb.velocity = new Vector2(rb.velocity.x, 0);
+                    //Move left
+                    rb.velocity = new Vector2(-SPEED, rb.velocity.y);
                 }
                 else
                 {
-                    rb.velocity = new Vector2(0,0);
+                    //Move right
+                    rb.velocity = new Vector2(SPEED, rb.velocity.y);
+                }
+            }
+            else if (angle > 22.5f)
+            {
+                if (getYDiff() > 0) //If the follower is above the player
+                {
+                    //Move down
+                    rb.velocity = new Vector2(rb.velocity.x, -SPEED);
+                }
+                else
+                {
+                    //Move up
+                    rb.velocity = new Vector2(rb.velocity.x, SPEED);
                 }
             }
         }
@@ -81,89 +71,30 @@ public class Follow : MonoBehaviour
 
     private bool toMove()
     {
-        if (getDist(player) > START_D)
+        if (getDist() > START_D)
         {
             return true;
         }
         else return false;
     }
 
-    private float getDist(GameObject target)
+    private float getDist()
     {
         return (this.transform.position - player.transform.position).magnitude;
     }
 
-    private float getDist(GameObject start, GameObject target)
+    private float getAngle() //Returns the angle measured counterclockwise from the x-axis.
     {
-        return (start.transform.position - player.transform.position).magnitude;
+        return Mathf.Acos(Mathf.Abs(getXDiff()) / getDist()) * 180 / Mathf.PI;
     }
 
-    private float getAngle(GameObject target) //Returns the angle measured counterclockwise from the x-axis.
+    private float getXDiff()
     {
-        return Mathf.Acos(Mathf.Abs(getXDiff(player)) / getDist(player)) * 180 / Mathf.PI;
+        return this.transform.position.x - player.transform.position.x;
     }
 
-    private float getXDiff(GameObject target)
+    private float getYDiff()
     {
-        return this.transform.position.x - target.transform.position.x;
-    }
-
-    private float getYDiff(GameObject target)
-    {
-        return this.transform.position.y - target.transform.position.y;
-    }
-
-    private bool[] walkDir(GameObject target)
-    {
-        bool[] dirs = new bool[4]; //Left=0, right=1, up=2, down=3
-        angle = getAngle(target);
-        if (angle < 67.5f)
-        {
-            if (getXDiff(target) > 0) //If the follower is to the right of the player
-            {
-                //Move left
-                dirs[0] = true;
-                dirs[1] = false;
-            }
-            else
-            {
-                //Move right
-                dirs[0] = false;
-                dirs[1] = true;
-            }
-        }
-        else
-        {
-            dirs[0] = false;
-            dirs[1] = false;
-        }
-        if (angle > 22.5f)
-        {
-            if (getYDiff(target) > 0) //If the follower is above the player
-            {
-                //Move down
-                dirs[2] = false;
-                dirs[3] = true;
-            }
-            else
-            {
-                //Move up
-                dirs[2] = true;
-                dirs[3] = false;
-            }
-        }
-        else
-        {
-            dirs[2] = false;
-            dirs[3] = false;
-        }
-        return dirs;
-    }
-
-    private bool isClosest()
-    {
-        if (getDist(player) < getDist(otherFollower, player))
-            return true;
-        else return false;
+        return this.transform.position.y - player.transform.position.y;
     }
 }
