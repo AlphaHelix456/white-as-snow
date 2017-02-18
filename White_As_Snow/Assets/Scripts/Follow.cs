945e88acf2be051648598ec0f9438ba552638dd1
InvalidCS <cjdipalma@yahoo.com>
2017-02-18 01:29:48 -0800

    Follow code changes

--- White_As_Snow/Assets/Scripts/Follow.cs
+++ White_As_Snow/Assets/Scripts/Follow.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class Follow : MonoBehaviour
 {
 
-    private readonly float START_D = 7.1f;
-    private readonly float MIN_D = 5.5f;
+    private readonly float START_D = 6.5f;
+    private readonly float MIN_D = 5f;
+    private readonly float FOLLOW_D = 4.2f;
     private readonly float SPEED = 4.5f;
     private GameObject player;
     private GameObject otherFollower;
     private Rigidbody2D rb;
-    private bool moving;
+    public bool moving, needsToMove;
     private float angle;
+    private bool[][] dirs = new bool[2][];
 
     void Start()
     {
+        for (int i = 0; i < 2; i++)
+            dirs[i] = new bool[4];
         player = GameObject.Find("wolf");
         moving = false;
+        needsToMove = false;
         rb = GetComponent<Rigidbody2D>();
         if (gameObject.name == "follower1")
         {
@@ -38,31 +43,36 @@ public class Follow : MonoBehaviour
 
         if (moving)
         {
-            angle = getAngle();
-            if (angle < 67.5f)
+            if (getDist(player) <= MIN_D)
             {
-                if (getXDiff() > 0) //If the follower is to the right of the player
-                {
-                    //Move left
-                    rb.velocity = new Vector2(-SPEED, rb.velocity.y);
-                }
-                else
-                {
-                    //Move right
-                    rb.velocity = new Vector2(SPEED, rb.velocity.y);
-                }
+                moving = false;
+                rb.velocity = new Vector2(0, 0);
             }
-            else if (angle > 22.5f)
+            else
             {
-                if (getYDiff() > 0) //If the follower is above the player
+                dirs[0] = walkDir(player);
+                dirs[1] = walkDir(otherFollower);
+
+                //Left=0, right=1, up=2, down=3
+                //Movement in direction of player unless there is another wolf in the way
+                if(getDist(otherFollower) > FOLLOW_D || )
                 {
-                    //Move down
-                    rb.velocity = new Vector2(rb.velocity.x, -SPEED);
+                    if (dirs[0][0])
+                        rb.velocity = new Vector2(-SPEED, rb.velocity.y);
+                    else if (dirs[0][1])
+                        rb.velocity = new Vector2(SPEED, rb.velocity.y);
+                    else
+                        rb.velocity = new Vector2(0, rb.velocity.y);
+                    if (dirs[0][2])
+                        rb.velocity = new Vector2(rb.velocity.x, SPEED);
+                    else if (dirs[0][3])
+                        rb.velocity = new Vector2(rb.velocity.x, -SPEED);
+                    else
+                        rb.velocity = new Vector2(rb.velocity.x, 0);
                 }
                 else
                 {
-                    //Move up
-                    rb.velocity = new Vector2(rb.velocity.x, SPEED);
+                    rb.velocity = new Vector2(0,0);
                 }
             }
         }
@@ -71,30 +81,89 @@ public class Follow : MonoBehaviour
 
     private bool toMove()
     {
-        if (getDist() > START_D)
+        if (getDist(player) > START_D)
         {
             return true;
         }
         else return false;
     }
 
-    private float getDist()
+    private float getDist(GameObject target)
     {
         return (this.transform.position - player.transform.position).magnitude;
     }
 
-    private float getAngle() //Returns the angle measured counterclockwise from the x-axis.
+    private float getDist(GameObject start, GameObject target)
     {
-        return Mathf.Acos(Mathf.Abs(getXDiff()) / getDist()) * 180 / Mathf.PI;
+        return (start.transform.position - player.transform.position).magnitude;
     }
 
-    private float getXDiff()
+    private float getAngle(GameObject target) //Returns the angle measured counterclockwise from the x-axis.
     {
-        return this.transform.position.x - player.transform.position.x;
+        return Mathf.Acos(Mathf.Abs(getXDiff(player)) / getDist(player)) * 180 / Mathf.PI;
     }
 
-    private float getYDiff()
+    private float getXDiff(GameObject target)
     {
-        return this.transform.position.y - player.transform.position.y;
+        return this.transform.position.x - target.transform.position.x;
+    }
+
+    private float getYDiff(GameObject target)
+    {
+        return this.transform.position.y - target.transform.position.y;
+    }
+
+    private bool[] walkDir(GameObject target)
+    {
+        bool[] dirs = new bool[4]; //Left=0, right=1, up=2, down=3
+        angle = getAngle(target);
+        if (angle < 67.5f)
+        {
+            if (getXDiff(target) > 0) //If the follower is to the right of the player
+            {
+                //Move left
+                dirs[0] = true;
+                dirs[1] = false;
+            }
+            else
+            {
+                //Move right
+                dirs[0] = false;
+                dirs[1] = true;
+            }
+        }
+        else
+        {
+            dirs[0] = false;
+            dirs[1] = false;
+        }
+        if (angle > 22.5f)
+        {
+            if (getYDiff(target) > 0) //If the follower is above the player
+            {
+                //Move down
+                dirs[2] = false;
+                dirs[3] = true;
+            }
+            else
+            {
+                //Move up
+                dirs[2] = true;
+                dirs[3] = false;
+            }
+        }
+        else
+        {
+            dirs[2] = false;
+            dirs[3] = false;
+        }
+        return dirs;
+    }
+
+    private bool isClosest()
+    {
+        if (getDist(player) < getDist(otherFollower, player))
+            return true;
+        else return false;
     }
 }
