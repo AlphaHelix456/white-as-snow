de2dfc2d99845ff8961e605c42a618f1d547e382
tolkwes <tolkwes@gmail.com>
2017-02-22 19:12:52 -0800

    Footstep Sound Code

--- White_As_Snow/Assets/Scripts/WolfMovement.cs
+++ White_As_Snow/Assets/Scripts/WolfMovement.cs
@@ -8,36 +8,53 @@ public class WolfMovement : MonoBehaviour {
     private readonly float SPEED = 5.3f;
     protected bool facingright = true;
     protected bool facingup = false;
+    public AudioSource footsteps;
+    private bool isMoving;
 
-	void Start () {
+    void Start () {
         rb = gameObject.GetComponent<Rigidbody2D>();
 	}
 	
 	void FixedUpdate () {
         //Movement
         if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
+            isMoving = true;
             rb.velocity = new Vector2(rb.velocity.x, SPEED);
             facingup = true;
         }
         else if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
+            isMoving = true;
             rb.velocity = new Vector2(rb.velocity.x, -SPEED);
             facingup = false;
         }
         else {
+            isMoving = false;
             rb.velocity = new Vector2(rb.velocity.x, 0);
         }
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
+            isMoving = true;
             rb.velocity = new Vector2(SPEED, rb.velocity.y);
             facingright = true;
             facingup = false;
         }
-        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+        else if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
+            isMoving = true;
             rb.velocity = new Vector2(-SPEED, rb.velocity.y);
             facingright = false;
             facingup = false;
         }
         else {
+            isMoving = false;
             rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+
+        if(isMoving == true) {
+            footsteps.enabled = true;
+            footsteps.loop = true;
+        }
+        else {
+            footsteps.enabled = false;
+            footsteps.loop = false;
         }
     }
 }
