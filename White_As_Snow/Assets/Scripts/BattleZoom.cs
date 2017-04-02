8766b6c92657bb02db5ae96869d25733d34ccb06
Jake Smith <jesmith24@comcast.net>
2017-04-01 17:17:31 -0700

    Battle zoom
    Epically zooms in on the battle before the combat starts.

---
+++ White_As_Snow/Assets/Scripts/BattleZoom.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BattleZoom : MonoBehaviour {
+
+    private Camera myCamera; 
+
+	// Use this for initialization
+	void Start () {
+        myCamera = this.gameObject.GetComponent<Camera>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (myCamera.orthographicSize > 5)
+            decreaseSize();
+        if (myCamera.orthographicSize < 5)
+        {
+            myCamera.orthographicSize = 5;
+        }
+    }
+    void decreaseSize()
+    {
+        myCamera.orthographicSize -= 5 * Time.deltaTime;
+    }
+}
